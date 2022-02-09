540a49fba3232c77184d40076e281b8933f880a9
Sophea Phy <sophea@web-essentials.co>
2022-02-09 17:42:58 +0700

    [TASK] Connect the new organization to the admin service
    refs HIG-15

--- src/services/educationMaterial.js
+++ src/services/educationMaterial.js
@@ -1,4 +1,4 @@
-import axios from 'utils/axios';
+import axios from 'utils/gAdminAxios';
 import _ from 'lodash';
 
 const getEducationMaterial = (id, language) => {
--- src/services/questionnaire.js
+++ src/services/questionnaire.js
@@ -1,4 +1,4 @@
-import axios from 'utils/axios';
+import axios from 'utils/gAdminAxios';
 import _ from 'lodash';
 
 const getQuestionnaires = payload => {
