47f84b99727695bf4453250c1e4dcaa1761c43ab
Rayuth You <rayuth@web-essentials.co>
2025-08-22 11:29:08 +0700

    [IMP] Add <> into password validate
    refs TRA-1074

--- src/utils/validatePassword.js
+++ src/utils/validatePassword.js
@@ -1,6 +1,6 @@
 const validatePassword = (password) => {
   // eslint-disable-next-line
-  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*<>])(?=.{8,})/;
   return re.test(String(password));
 };
 
