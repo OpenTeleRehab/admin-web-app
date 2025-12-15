9f8ee1fc220640339a7ebfab5acad67baadabf9b
Sokleap Seng <sokleap@web-essentials.co>
2025-12-15 17:12:21 +0700

    [TASK] Improve therapist and Phc worker limit
    refs TRA-1105-IMP

--- src/interfaces/ILimitation.ts
+++ src/interfaces/ILimitation.ts
@@ -1,6 +1,8 @@
 export interface ILimitation {
   therapist_limit_used: number;
   remaining_therapist_limit: number;
+  allocated_therapist_limit: number;
   phc_worker_limit_used: number;
   remaining_phc_worker_limit: number;
+  allocated_phc_worker_limit: number;
 }
